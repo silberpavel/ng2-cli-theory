d3d16106d605862c469e0e05eae3b70af87f16ef
Silber <silber.ypm@gmail.com>
2018-11-13 18:20:21 +0200

    text in input and than Enter
    Закончить отображение  в "p" по нажатию text in input and than Enter

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommandExecutor } from 'selenium-webdriver/safari';
 
 @Component({
   //              ===== SELECTORS ===== 
@@ -12,11 +13,44 @@ import { Component } from '@angular/core';
 export class CarsComponent {
 
   canAddCar = false;
+  inputText = '';
 
   constructor() {
     setTimeout( () => {
       this.canAddCar = true;
     },2000);
   }
+
+  addCarsStatus = '';
+  addCar() {
+    this.addCarsStatus = "Car added!"
+  }
+  
+  // onKeyUp(event:Event) {
+  //   this.inputText = (<HTMLInputElement>event.target).value;
+  // }
+
+// <!--Способ номер №2  -- >   
+  // onKeyUp(event) {
+  //     this.inputText = event;
+  // }
+
+// <!--Способ номер №2    Еще добавили отображение после нажатия Enter -- >   
+  onKeyUp(event) {
+    // console.log(event);
+    this.inputText = event;
+    if (event.code === 'g') {
+      console.log(event);
+      
+      this.inputText = event.target.value;
+    }
+  }
+
+
+
+
+
+
+
 
 }
