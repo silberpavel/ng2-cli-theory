ce00801fff810e3374b4d67f4802c205868d5221
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-11-18 15:25:35 +0200

    Update

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -6,5 +6,10 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent {
-  @Input() carItem:{name:string, year:number};
+  @Input('carItem') car:{name:string, year:number};
+
+    private carItem = false;
+
+    // Существуют Alias, когда хотим поменять название...
 }
+  
