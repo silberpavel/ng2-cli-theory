81cfafc3a9b553702430e398596859394100c496
Silber <silber.ypm@gmail.com>
2018-11-25 16:38:55 +0200

    Update background.directive.ts

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2, asNativeElements } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2, asNativeElements, HostListener } from "@angular/core";
 
 @Directive({
     selector: '[appBackground]'
@@ -11,12 +11,26 @@ export class BackgroundDirective implements OnInit {
 
 
     ngOnInit() {
-        const {nativeElement} = this.element;
-        this.renderer.setStyle(nativeElement, 'background-color', 'LightBlue');
-        this.renderer.addClass(this.element.nativeElement, 'white-text');
-;
+
         // console.log(this.element);
         // this.element.nativeElement.style.backgroundColor = 'LightGreen';
         
+    }
+
+    // @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
+    //         console.log(event); 
+    //     }
+
+
+    @HostListener('mouseenter') mouseEnter() {
+        const { nativeElement } = this.element;
+        this.renderer.setStyle(nativeElement, 'background-color', 'Green');
+        this.renderer.addClass(this.element.nativeElement, 'white-text');
+    }
+    @HostListener('mouseleave') mouseLeave() {
+        const { nativeElement } = this.element;
+        this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
+        this.renderer.removeClass(this.element.nativeElement, 'white-text');
+
     }
 }
