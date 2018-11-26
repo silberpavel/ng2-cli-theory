059b153f94b4e18fbbdafe26f24c0f4b08961508
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-11-26 10:26:07 +0200

    Pipes is awesome!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="col-xs-8 col-xs-offset-2">
 
+    <!--
       <h2>{{ name }}</h2>
       <h2>{{ name | lowercase }}</h2>
       <h2>{{ name | uppercase }}</h2>
@@ -17,7 +18,51 @@ import { Component } from '@angular/core';
 
       <h2>{{ pi }}</h2>
       <h2>{{ pi | number }}</h2>
+      <h2>{{ pi | number:'1.3-4' }}</h2>
       <h2>{{ pi | number:'2.2-2' }}</h2>
+
+      <hr>
+
+      <h2>{{ money | currency }}</h2>
+      <h2>{{ money | currency: 'USD' }}</h2>
+      <h2>{{ money | currency: 'USD': false }}</h2>
+
+      <h2>{{ money | currency: 'EUR' }}</h2>
+      <h2>{{ money | currency: 'EUR': false }}</h2>
+
+      <h2>{{ money | currency: 'NIS' }}</h2>
+
+      <h2>{{ money | currency: 'RUB' }}</h2>
+
+      <hr>
+    -->
+
+      <h2>{{ date }}</h2>
+      <p>here is pipe below</p>
+      <h2>{{ date | date }}</h2>
+      <h2>{{ date | date: 'fullDate' }}</h2>
+      <h2>{{ date | date: 'shortDate' }}</h2>
+      <h2>{{ date | date: 'fullTime' }}</h2>
+      <h2>{{ date | date: 'shortTime' }}</h2>
+
+      <a href="https://angular.io/api?query=pipe">API List (Pipes)</a>
+
+
+      <hr>
+
+      <h2>{{ amount }}</h2>
+      <h2>{{ amount | percent }}</h2>
+
+      <hr>
+
+      <h2>{{ object }}</h2>
+      <pre>{{ object | json }}</pre>
+
+
+
+
+
+
 
     </div>
   `
