cd2dbe37da81fb0fafacdf1db4ee3989ad0f7b62
Silber <silber.ypm@gmail.com>
2018-11-26 16:58:26 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,82 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="col-xs-8 col-xs-offset-2">
-
-    <!--
-      <h2>{{ name }}</h2>
-      <h2>{{ name | lowercase }}</h2>
-      <h2>{{ name | uppercase }}</h2>
-      <h2>{{ name | slice:0:3 }}</h2>
-      <h2>{{ name | slice:3:6 }}</h2>
-      <h2>{{ name | slice:8 }}</h2>
-      <h2>{{ name | slice:8 | uppercase}}</h2>
-
-      <hr>
-
-      <h2>{{ pi }}</h2>
-      <h2>{{ pi | number }}</h2>
-      <h2>{{ pi | number:'1.3-4' }}</h2>
-      <h2>{{ pi | number:'2.2-2' }}</h2>
-
-      <hr>
-
-      <h2>{{ money | currency }}</h2>
-      <h2>{{ money | currency: 'USD' }}</h2>
-      <h2>{{ money | currency: 'USD': false }}</h2>
-
-      <h2>{{ money | currency: 'EUR' }}</h2>
-      <h2>{{ money | currency: 'EUR': false }}</h2>
-
-      <h2>{{ money | currency: 'NIS' }}</h2>
-
-      <h2>{{ money | currency: 'RUB' }}</h2>
-
-      <hr>
-    -->
-
-      <h2>{{ date }}</h2>
-      <p>here is pipe below</p>
-      <h2>{{ date | date }}</h2>
-      <h2>{{ date | date: 'fullDate' }}</h2>
-      <h2>{{ date | date: 'shortDate' }}</h2>
-      <h2>{{ date | date: 'fullTime' }}</h2>
-      <h2>{{ date | date: 'shortTime' }}</h2>
-
-      <a href="https://angular.io/api?query=pipe">API List (Pipes)</a>
-
-
-      <hr>
-
-      <h2>{{ amount }}</h2>
-      <h2>{{ amount | percent }}</h2>
-
-      <hr>
-
-      <h2>{{ object }}</h2>
-      <pre>{{ object | json }}</pre>
-
-
-
-
-
-
+      <h2>{{ num }}</h2>
+      <h2>{{ num | appPow:3 }}</h2>
 
     </div>
   `
 })
 export class AppComponent {
-  name = 'WebForMyself';
-  pi = Math.PI;
-  money = 350;
-  date = new Date();
-  amount = 0.45;
-  object = {
-    foo: 'bar',
-    baz: 'qux',
-    nested:
-      {
-        xyz: 3,
-        numbers: [1, 2, 3]
-      }
-  };
+  num = 2;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { PowPipe } from "./pow.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PowPipe
   ],
   imports: [
     BrowserModule,
