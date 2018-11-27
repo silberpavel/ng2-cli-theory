f197b8c4cfa5691874c5590a6b170eafc06fbeb3
Silber <silber.ypm@gmail.com>
2018-11-27 18:23:55 +0200

    delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+import { delay } from "rxjs/operators";
+import { of } from "rxjs";
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="col-xs-8 col-xs-offset-2">
+
+      <h1>{{ title }}</h1>
+
       <input type="text" class="form-control" [(ngModel)]="searchCar">
       <button class="btn btn-primary" (click)="addCar()">Добавить</button>
       <hr>
@@ -27,6 +36,12 @@ export class AppComponent {
     { name: 'Mercedes-Benz', descr: 'WFM 5'},
     { name: 'BMW', descr: 'WFM 6'},
   ];
+
+  title ='';
+  // RXJS - выучить...
+  asyncTitle = of('Async title 3 seconds')
+  .pipe(delay(3000))
+  .subscribe((str) => this.title = str);
 
   addCar() {
     this.cars.push({
