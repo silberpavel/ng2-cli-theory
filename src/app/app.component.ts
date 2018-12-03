f64650ac4add6ab8e304eaa996c29e3a844ca84f
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-12-03 12:01:58 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,29 @@
 import { Component } from '@angular/core';
-import 'rxjs/Rx';
-import { Observable } from 'rxjs/observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
-import { delay } from "rxjs/operators";
-import { of } from "rxjs";
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="col-xs-8 col-xs-offset-2">
-
-      <h1>{{ asyncTitle | async }}</h1>
-
-      <input type="text" class="form-control" [(ngModel)]="searchCar">
-      <button class="btn btn-primary" (click)="addCar()">Добавить</button>
-      <hr>
-      <ul class="list-group">
-        <li class="list-group-item"
-
-        *ngFor="let car of cars | carFilter: searchCar: 'name'; let i = index"
-        ><b>{{ i + 1 }}</b> {{ car.name }} <i>{{ car.descr }}</i></li>
-
-      </ul>
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  searchCar = '';
   cars = [
-    {name: 'Ford', descr: 'WFM 1'},
-    { name: 'Mazda', descr: 'WFM 2'},
-    { name: 'Bently', descr: 'WFM 3'},
-    { name: 'Audi', descr: 'WFM 4'},
-    { name: 'Mercedes-Benz', descr: 'WFM 5'},
-    { name: 'BMW', descr: 'WFM 6'},
+    {
+      name: 'Ford',
+      isSold: false
+    },
+    {
+      name: 'Mazda',
+      isSold: true
+    },
+    {
+      name: 'Mercedes',
+      isSold: false
+    }
   ];
 
-  // title =''; // to display
-  // RXJS - выучить...
-  asyncTitle = of('Async title 3 seconds')
-  .pipe(delay(3000))
-  // .subscribe((str) => this.title = str);
-
-  addCar() {
+  addCarToList(carName: string) {
     this.cars.push({
-      name: 'New Car',
-      descr: 'WFM'
+      name: carName,
+      isSold: false
     });
   }
 }
-
-
-
-// Чтобы создать Pipe: 
-// 1. Мы создам класс который имплементится от PipeTransform
-// export class PowPipe implements PipeTransform
-// Интерфейс которого так же импортитуется из '@angular/core'
-// import { Pipe, PipeTransform } from '@angular/core';
-// 2. В декораторе @Pipe (имя Пайпа, that wr use in template): указываем name: 'appPow'
-// 3. Registration in @NgModule  (app.models.ts)  
-// AND NOW, WE CAN USE IT!
-
-// PIPE CLI:  ng g p my-test-pipe
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
-import { PowPipe } from "./pow.pipe";
-import { CarFilterPipe } from './car-filter.pipe';
+import { CarComponent } from './car/car.component';
+import { CarAddComponent } from './car-add/car-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PowPipe,
-    CarFilterPipe
+    CarComponent,
+    CarAddComponent
   ],
   imports: [
     BrowserModule,
