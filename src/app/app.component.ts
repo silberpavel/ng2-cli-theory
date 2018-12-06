4d6032e7532137aca07baf65a261c063d980064f
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-12-06 11:55:38 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'app-root',  // located in main html       
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ng2-cli-theory';
+  answers = [{
+    type: 'yes',
+    text: 'Да'
+  }, {
+    type: 'no',
+    text: 'Нет'
+  }];
 }
