a302b2480a714a73951be0cb0cdcb559f2ef2dcc
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-12-12 14:48:03 +0200

    Update cars.service.ts

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
 
 
-@Injectable
+@Injectable()
 export class CarsService {
+    constructor(private http: Http) {}
 
+    getCars() {
+      this.http.get();  
+    }
 }
